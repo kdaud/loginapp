26c584781c04bac7e1683c7ea3db946a5eb8f28e
kdaud <dkakumirizii@gmail.com>
2020-12-03 17:47:40 +0300

    Login codes

---
+++ login/myscript.js
@@ -0,0 +1,9 @@
+//alert("OpenMRS is a Medical Record System");
+//alert('Am a google commiter contributing codes to the OpenMRS code base')
+document.body.innerHTML ="<h4 style='color:purple'>BS. IN SOFTWARE ENGINEERING <hr> </h4>";
+
+document.write(" Programmers of tommorow will be the wizzards of the future. People will gonna think "+
+"you have magic <br>powers compared to any one else. You don't need to be genuis to know how to code"+
+"you need to be de-<br>termined </b>");
+document.write("<hr>");
+console.timeStamp();
